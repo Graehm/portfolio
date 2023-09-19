8c28499aa524dc5f22b8776857c269dac27daed8
Graehm <gpatrellfazio@gmail.com>
2023-09-18 21:04:54 -0500

    porting server through env for best practice

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const PORT = process.env.PORT || 3008
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+
 
 // server used to send send emails
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(3008, () => console.log("Server Running"));
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
+// app.listen(3008, () => console.log("Server Running"));
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
 
