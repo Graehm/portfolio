af3218ddd8f5fc52fbc151d0f73119fa81e07adb
Graehm <gpatrellfazio@gmail.com>
2023-09-24 21:52:26 -0500

    finalized nav bar

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Graehm,`}</h1>
-                <h1><span className="txt-rotate" dataPeriod="1000" data-rotate='[ "A Web Developer" ]'><span className="wrap">{text}</span></span></h1>
+                <h1>{`A `}<span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer" ]'><span className="wrap">{text}</span></span></h1>
                   <p>
                     Campaign strategist and business operations wiz turned Full Stack Software Developer. Adaptable and quick to pick up both technical and project-oriented processes - I know how to work with a variety of teams on a range of projects. 
                     I thrive in collaborative teams that require a resource-conscious developer who thinks out of the box.
