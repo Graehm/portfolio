57cb5b83c5626cf42f6ed4ac032e64fac1116a39
Graehm <gpatrellfazio@gmail.com>
2023-09-27 19:18:52 -0500

    linked projects successful but messed with css so will need to fix that

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,13 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+
 
 export const Projects = () => {
 
@@ -14,20 +16,24 @@ export const Projects = () => {
       title: "Tic Tac Toe",
       description: "OOP structure",
       imgUrl: projImg1,
+      link: 'https://exquisite-nougat-5d8b25.netlify.app/'
     },
     {
       title: "BimmerBits",
       description: "on online automotive parts supplier",
       imgUrl: projImg2,
+      link: 'https://bimmerbits.graehm.me/'
     },
     {
       title: "Fallmart",
       description: "An e commerce MERN Stack app",
       imgUrl: projImg3,
+      link: 'https://fallmart.graehm.me/'
     },
   ];
 
   return (
+    <Router>
     <section className="project" id="projects">
       <Container>
         <Row>
@@ -38,18 +44,22 @@ export const Projects = () => {
                 <h2>Projects</h2>
                 <p>Preview and view my deployed projects below</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                  <Nav 
+                    variant="pills" 
+                    className="nav-pills mb-5 justify-content-center align-items-center" 
+                    id="pills-tab">
                   </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                  <Tab.Content 
+                    id="slideInUp" 
+                    className={isVisible ? "animate__animated animate__slideInUp" : ""}
+                  >
                     <Tab.Pane eventKey="first">
                       <Row>
-                        {
-                          projects.map((project, index) => {
+                        {projects.map((project, index) => {
                             return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
-                                />
+                              <Link key={index} to={project.link}>
+                                <ProjectCard {...project} />
+                              </Link>
                             )
                           })
                         }
@@ -64,5 +74,6 @@ export const Projects = () => {
       </Container>
       <img className="background-image-right" src={colorSharp2} alt=''></img>
     </section>
+    </Router>
   )
 }
