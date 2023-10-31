f89ed57697eac60afd1cc7e2870e981dda4f1bd0
Graehm <gpatrellfazio@gmail.com>
2023-10-31 16:14:38 -0500

    text min width better for mobile

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -58,7 +58,7 @@ export const Projects = () => {
                             projects.map((project, index) => {
                               return (
                                 <div style={{ border: "" }}>
-                                  <Link key={index} to={project.link} style={{ width: "max-content" }}>
+                                  <Link key={index} to={project.link} style={{ width: "min-content" }}>
                                     <ProjectCard {...project} />
                                   </Link>
                                 </div>
