0fc3995e0a653f3b03c31e9c904bffbb6a195382
Graehm <gpatrellfazio@gmail.com>
2023-10-31 15:41:35 -0500

    project card component without boostrap Col

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,7 +2,6 @@ import { Col } from "react-bootstrap";
 
 export const ProjectCard = ({ title, description, imgUrl }) => {
   return (
-    <Col size={12} sm={6} md={4}>
       <div className="proj-imgbx">
         <img src={imgUrl} alt=""/>
         <div className="proj-txtx">
@@ -10,6 +9,19 @@ export const ProjectCard = ({ title, description, imgUrl }) => {
           <span>{description}</span>
         </div>
       </div>
-    </Col>
   )
 }
+
+// export const ProjectCard = ({ title, description, imgUrl }) => {
+//   return (
+//     <Col size={12} sm={6} md={4}>
+//       <div className="proj-imgbx">
+//         <img src={imgUrl} alt=""/>
+//         <div className="proj-txtx">
+//           <h4>{title}</h4>
+//           <span>{description}</span>
+//         </div>
+//       </div>
+//     </Col>
+//   )
+// }
