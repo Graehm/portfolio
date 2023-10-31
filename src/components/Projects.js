68ae604542a1ceb993edce2d6036f438d0516200
Graehm <gpatrellfazio@gmail.com>
2023-10-31 16:04:43 -0500

    re wording

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -43,7 +43,7 @@ export const Projects = () => {
                 {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn": ""} >
                   <h2>Projects</h2>
-                  <p>View my deployed projects here</p>
+                  <p>View my deployed projects</p>
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab"></Nav>
                     <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
